85ab67d0f01c7415b4852968865216b8c84c82ec
Merve <gulineyel@gmail.com>
2020-10-10 19:24:09 +0300

    bilgi

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,15 @@
   
 export * from './Button'
 export * from './Input'
+
+
+// :mega:Önceki Bölümde Neler Oldu:
+// :point_right:Fonksiyon vs Fonksiyon ()
+// :point_right:Spread operator
+// :point_right:Destruction operatör
+// :point_right:Debug açma
+// :point_right:Transparent icon search
+// :point_right:Dimension
+// :point_right:İmport & export (çoklu)
+// :point_right:Platform.OS (ternary for  padding)
+// :point_right:Keyboardavoidinview  & scrollview
