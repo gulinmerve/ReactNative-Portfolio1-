27deddebd207df1a4ba934bcff32c04bdd0928ce
Merve <gulineyel@gmail.com>
2020-10-12 08:18:19 +0300

    nes page added

---
+++ src/src-NewsPage-InClass/index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import Login from './src/Login';
+import News from './src/News';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => News);
